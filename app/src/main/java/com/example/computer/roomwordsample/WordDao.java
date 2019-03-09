82da032742c6aab00aca05d029df2e7b51edec90
segunfrancis <francis4dblues@gmail.com>
2019-03-09 23:05:19 +0100

    Update DAO

--- app/src/main/java/com/example/computer/roomwordsample/WordDao.java
+++ app/src/main/java/com/example/computer/roomwordsample/WordDao.java
@@ -6,6 +6,11 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+/* The DAO must be an interface or an abstract class
+*  By default, all queries bust be executed on a separate thread
+*  Room uses the DAO to create a clean API for your code.
+*/
+
 @Dao
 public interface WordDao {
 
